f51da27b6de48719261fc8db5eb606e8a4fe634c
Thaddeus Lee <thaddeuslee@Thaddeuss-MacBook-Pro.local>
2023-12-15 01:14:36 +0800

    Minor Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+"use client";
+
 import Image from "next/image";
 import { Button } from "../components/ui/button";
+import { Bars3BottomLeftIcon } from "@heroicons/react/24/solid";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Hello</Button>
-    </main>
+    <>
+      <div className="w-screen flex justify-end items-center">
+        <Bars3BottomLeftIcon className="w-10 aspect-square" />
+        <MagnifyingGlassIcon className="w-10 aspect-square" />
+      </div>
+    </>
   );
 }
