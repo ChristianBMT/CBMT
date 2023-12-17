61a390474aed4261c4a6a60a46d5a60605692648
Soh Hong Yu <sohhongyu@gmail.com>
2023-12-18 02:00:26 +0800

    Added Vercel SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Loading } from "@/components/Loading";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
         >
           <Loading />
           {children}
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
