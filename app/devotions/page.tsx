08ff699abd9fc5f94f5763deb6094ad2c425d445
Soh Hong Yu <sohhongyu@gmail.com>
2024-01-02 03:21:55 +0800

    Update Devotion Page

--- app/devotions/page.tsx
+++ app/devotions/page.tsx
@@ -1,14 +1,9 @@
+"use client";
+
 import { Devotion } from "@/types";
 import { columns } from "@/components/table/columns";
 import { DataTable } from "@/components/table/DataTable";
-
-async function getData(): Promise<Devotion[]> {
-  let response = await fetch(
-    process.env.NEXT_PUBLIC_SERVER_URL + "/api/devotions"
-  );
-  let data: Devotion[] = await response.json();
-  return data;
-}
+import { useState, useEffect } from "react";
 
 type Week = {
   week: number;
@@ -16,20 +11,34 @@ type Week = {
   date: string;
 };
 
-async function getWeek(): Promise<Week[]> {
-  let response = await fetch(process.env.NEXT_PUBLIC_SERVER_URL + "/api/week");
-  let data: Week[] = await response.json();
-  console.log(data);
-  return data;
-}
+export default function DevotionPage() {
+  const [allDevotion, setAllDevotion] = useState<Devotion[]>([]);
+  const [week, setWeek] = useState<Week[]>([]);
+  async function getData() {
+    let response = await fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/api/devotions`
+    );
+    let data = await response.json();
+    setAllDevotion(data);
+  }
 
-export default async function DevotionPage() {
-  const data = await getData();
-  const week = await getWeek();
+  async function getWeek() {
+    let response = await fetch(
+      process.env.NEXT_PUBLIC_SERVER_URL + "/api/week"
+    );
+    let data: Week[] = await response.json();
+    setWeek(data);
+  }
+
+  useEffect(() => {
+    getData();
+    getWeek();
+  }, []);
+
   return (
     <main className="min-h-[calc(100dvh-48px)] flex flex-col mx-auto max-w-[500px] px-6 pb-6">
       <h1 className="font-semibold text-xl text-center">All Devotions</h1>
-      <DataTable columns={columns} data={data} weekData={week} />
+      <DataTable columns={columns} data={allDevotion} weekData={week} />
     </main>
   );
 }
